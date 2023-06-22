a8d98b6ba9de1c91755067282eb544ecc89400d5
Wb <jinnseitanosi@gmail.com>
2023-06-22 22:38:15 +0900

    1:01:17

--- leetcode-yt/src/components/Modals/Signup.tsx
+++ leetcode-yt/src/components/Modals/Signup.tsx
@@ -1,6 +1,10 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useState, useEffect } from 'react';
+import { auth } from '@/firebase/firebase';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
 
 type SignupProps = {
     
@@ -12,18 +16,36 @@ const Signup:React.FC<SignupProps> = () => {
         setAuthModalState((prev) => ({ ...prev, type: "login"}));
     };
     const [inputs, setInputs] = useState({email:'',displayName:'',password:''});
+    const router = useRouter();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error
+    ] = useCreateUserWithEmailAndPassword(auth);
 
     const handleChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
         setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value}));
-    }
+    };
 
-    const handleRegister = (e:React.FormEvent<HTMLFormElement>) => {
+    const handleRegister = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(inputs);
-    }
+        if(!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields");
+        try {
+            const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+            if(!newUser) return;
+            router.push('/');
+        } catch (error:any) {
+            alert(error.message);
+        }
+    };
+
+    useEffect(() => {
+        if(error) alert(error.message)
+    }, [error]);
 
     return <>
-        <form className="space-y-6 px-6 pb-4" onSumbit={handleRegister}>
+        <form className="space-y-6 px-6 pb-4" onSubmit={handleRegister}>
             <h3 className='text-xl font-medium text-white'>Register to LeetClone</h3>
             <div>
                 <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
@@ -40,7 +62,7 @@ const Signup:React.FC<SignupProps> = () => {
                 <label htmlFor='displayName' className='text-sm font-medium block mb-2 text-gray-300'>
                     Display Name
                 </label>
-                <input type="displayName" name="displayName" id="displayName" className='
+                <input onChange={handleChangeInput} type="displayName" name="displayName" id="displayName" className='
                     border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                     bg-gray-600 border-gray-500 placeholder-gray-400 text-white
                 '
@@ -51,7 +73,7 @@ const Signup:React.FC<SignupProps> = () => {
                 <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
                     Password
                 </label>
-                <input type="password" name="password" id="password" className='
+                <input onChange={handleChangeInput} type="password" name="password" id="password" className='
                     border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                     bg-gray-600 border-gray-500 placeholder-gray-400 text-white
                 '
@@ -61,7 +83,7 @@ const Signup:React.FC<SignupProps> = () => {
             <button type='submit' className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
                 text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
             '>
-                Register
+                {loading ? "Registering..." : "Register"}
             </button>
             <div className='text-sm font-medium text-gray-300'>
                 Already have an account?{' '}
