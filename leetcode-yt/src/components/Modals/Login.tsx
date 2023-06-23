111ef0979760a0e2481501a5429f13a1c53dd549
Kuroakira <jinnseitanosi@gmail.com>
2023-06-23 19:01:45 +0900

    1:04:10

--- leetcode-yt/src/components/Modals/Login.tsx
+++ leetcode-yt/src/components/Modals/Login.tsx
@@ -1,5 +1,8 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import React from 'react';
+import { auth } from "@/firebase/firebase";
+import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useSetRecoilState } from 'recoil';
 
 type LoginProps = {
@@ -11,15 +14,40 @@ const Login:React.FC<LoginProps> = () => {
     const handleClick = (type:"login" | "register" | "forgotPassword") => {
         setAuthModalState((prev) => ({ ...prev, type}));
     };
-    
+    const [inputs, setInputs] = useState({ email:"", password: ""});
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const router = useRouter();
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value}));
+    };
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(!inputs.email || !inputs.password) return alert("Please fill all fields");
+        try {
+            const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
+            if(!newUser) return;
+            router.push("/");
+        } catch (error:any) {
+            alert(error.message);
+        }
+    };
+    console.log(user, "user");
+
     return <>
-        <form className="space-y-6 px-6 pb-4">
+        <form className="space-y-6 px-6 pb-4" onSubmit={handleLogin}>
             <h3 className='text-xl font-medium text-white'>Sign in to LeetClone</h3>
             <div>
                 <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
                     Your Email
                 </label>
-                <input type="email" name="email" id="email" className='
+                <input 
+                    onChange={handleInputChange}
+                    type="email" name="email" id="email" className='
                     border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                     bg-gray-600 border-gray-500 placeholder-gray-400 text-white
                 '
@@ -30,7 +58,9 @@ const Login:React.FC<LoginProps> = () => {
                 <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
                     Your Password
                 </label>
-                <input type="password" name="password" id="password" className='
+                <input 
+                    onChange={handleInputChange}
+                    type="password" name="password" id="password" className='
                     border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                     bg-gray-600 border-gray-500 placeholder-gray-400 text-white
                 '
